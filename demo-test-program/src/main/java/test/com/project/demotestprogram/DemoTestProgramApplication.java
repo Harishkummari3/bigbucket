7ca1e34310a799c9065e4741b48c8d8b2c1a351c
M1082798 <M1082798@MT-PF1WABZ4>
2022-12-13 17:30:55 +0530

    commiting the files

---
+++ demo-test-program/src/main/java/test/com/project/demotestprogram/DemoTestProgramApplication.java
@@ -0,0 +1,14 @@
+package test.com.project.demotestprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTestProgramApplication.class, args);
+		System.out.print("hiiiiii");
+	}
+
+}
---
+++ demo-test-program/src/test/java/test/com/project/demotestprogram/DemoTestProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package test.com.project.demotestprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTestProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
